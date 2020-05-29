bb357c79ce957dbf3e73226d5ad555a8ea481fa9
Giriraj Hiranandani <tghira16@gmail.com>
2020-05-29 13:30:12 +1000

    Added Timer and Listlike classes

--- utils.py
+++ utils.py
@@ -1,5 +1,23 @@
 import numpy as np
 from math import cos, sin
+import time
+class Timer:
+    def __init__(self, text="Elapsed time: {:0.4f} seconds", logger=print):
+        self._start_time = None
+        self.text = text
+        self.logger = logger
+    def start(self, label="task"):
+        self.label = label
+        self.t1 = time.time()
+    def stop(self):
+        self.t2 = time.time()
+        print("Time lapsed for %s: %s" % (self.label, self.t2-self.t1))
+
+def listlike(var):
+    if isinstance(var, list):
+        return var
+    else:
+        return [var]
 
 
 def TopHeavy(self, i, k=4):
