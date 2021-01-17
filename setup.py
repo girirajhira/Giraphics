5bf202661f62ea37070884a3f9efb8b90623d396
Giriraj Hiranandani <tghira16@gmail.com>
2021-01-18 01:19:35 +1100

    __

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Giraphics',         # How you named your package folder (MyLib)
   packages = ['giraphics'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Lightweight graphing and animations',   # Give a short description about your library
   author = 'T. G. Hiranandani',                   # Type in your name
